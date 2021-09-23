e4514a05fcc3852c1793efcbbfdba07a2547272f
Kevin Cruz <kocruz.24@gmail.com>
2021-09-23 17:30:18 -0400

    [105] Finishing the Chart Bars

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -27,7 +27,7 @@ class Chart extends StatelessWidget {
       print('WeekDay:${DateFormat.E().format(weekDay)}\nAmount:$totalSum');
 
       return {
-        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'day': DateFormat.E().format(weekDay).substring(0, 3),
         'amount': totalSum,
       };
     });
@@ -45,18 +45,25 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          var currentDaySpending = (data['amount'] as double) / totalSpending;
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            var currentDaySpending = (data['amount'] as double) / totalSpending;
 
-          if (totalSpending == 0.0) currentDaySpending = 0.0;
+            if (totalSpending == 0.0) currentDaySpending = 0.0;
 
-          return ChartBar(
-            data['day'],
-            data['amount'],
-            currentDaySpending,
-          );
-        }).toList(),
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                data['day'],
+                data['amount'],
+                currentDaySpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
